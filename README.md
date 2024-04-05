4d2dd6a03706e25e31c819541dc5da9579a16bc6
Kolja Wilcke <kolja@apfel.de>
2024-04-05 10:42:15 +0200

    always create a unique named pipe first -- and delete it after usage

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ using [lazy.nvim](https://github.com/folke/lazy.nvim):
 ```lua
     use {
         'kolja/loriini.nvim',
+        cmd = 'Loriini',
+        dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             require'loriini.nvim'.setup({
                 bin = '/usr/local/loriini', -- path to loriini executable
--- lua/loriini/init.lua
+++ lua/loriini/init.lua
@@ -1,11 +1,15 @@
 
 local M = {}
 
+local Job = require('plenary.job')
+
 M.setup = function(opts)
   M.opts = opts
 end
 
-local Job = require('plenary.job')
+local id = function()
+  return tostring({}):sub(10)
+end
 
 local win_opts = {
   relative = 'cursor',  -- win, editor, cursor
@@ -18,10 +22,12 @@ local win_opts = {
 }
 
 M.pick = function()
+  local pipe = '/tmp/loriini.'..id()
+  os.execute('mkfifo '..pipe)
   local BUF = vim.api.nvim_get_current_buf()
   local LISTEN = Job:new({
     command = '/bin/dd',
-    args = { 'if=/tmp/loriini', 'bs=7' },
+    args = { 'if='..pipe, 'bs=7' },
     interactive = false,
     on_stderr = function(_, data, _)
       print(data)
@@ -29,6 +35,7 @@ M.pick = function()
     on_exit = function(job, _, _)
       vim.schedule(function()
         job:shutdown()
+        os.execute('rm -f '..pipe)
       end)
     end,
     on_stdout = function(_, data, _)
@@ -46,18 +53,21 @@ M.pick = function()
     end,
   }):start()
 
+  local loriini = ''
   local line = vim.api.nvim_get_current_line()
   local color_pattern = "#%x+"
   local start_pos, end_pos = line:find(color_pattern, 0, false)
   if (start_pos and end_pos) then
-    M.opts.bin = M.opts.bin .. " -c " .. line:sub(start_pos + 1, end_pos)
+    loriini = M.opts.bin .. " -c " .. line:sub(start_pos + 1, end_pos) .. " -p " .. pipe
+  else
+    loriini = M.opts.bin .. " -p " .. pipe
   end
 
   local BUFNR = vim.api.nvim_create_buf(false, true)
   local WINNR = vim.api.nvim_open_win(BUFNR, true, win_opts)
   vim.api.nvim_feedkeys("i", "n", false)
 
-  vim.fn.termopen(M.opts.bin, {
+  vim.fn.termopen(loriini, {
     on_exit = function(_, _, _)
       vim.api.nvim_win_close(WINNR, true)
     end
